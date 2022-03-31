f15e5ce3ea2ddf75e89bd4a84cd2ac73b21a9a38
Garrett Sutula <garrettsutula@gmail.com>
2022-03-30 23:28:33 -0400

    Update mug with correct timing

--- src/crimes/mug.ts
+++ src/crimes/mug.ts
@@ -3,6 +3,6 @@ import { NS } from '@ns'
 export async function main(ns: NS): Promise<void> {
   while (true) {
     ns.commitCrime('mug someone');
-    await ns.sleep(65000);
+    await ns.sleep(4500);
   }
 }
