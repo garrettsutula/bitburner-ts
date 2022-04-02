e05e7f329131eb711adcf1a0c83e68cebc19232b
Garrett Sutula <garrettsutula@gmail.com>
2022-04-02 16:43:47 -0400

    Add logging function

--- src/lib/file.ts
+++ src/lib/file.ts
@@ -30,6 +30,10 @@ export function readJson(ns: NS, filePath: string): GenericObject | string[] {
   }
 }
 
-export function writeJson(ns: NS, filePath: string, fileContents: GenericObject | string[]): Promise<void> {
-  return ns.write(filePath, JSON.stringify(fileContents, replacer), 'w');
+export function writeJson(ns: NS, filePath: string, fileContents: GenericObject | string[], mode: ('w'|'a') = 'w'): Promise<void> {
+  return ns.write(filePath, JSON.stringify(fileContents, replacer), mode);
+}
+
+export function appendLog(ns: NS, filePath: string, logEntries:  GenericObject[]): Promise<void> {
+  return ns.write(filePath, logEntries.map((logEntry) => JSON.stringify(logEntry)).join('\n'), 'a');
 }
