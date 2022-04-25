c023a381d50bae4cfa3ee49ccca59bdaf5751768
Garrett Sutula <garrettsutula@gmail.com>
2022-04-24 23:17:28 -0400

    update sell hashes script to take param

--- src/sell-hashes.ts
+++ src/sell-hashes.ts
@@ -1,17 +1,22 @@
 import { NS } from '@ns';
-const sellForMoney = 'Sell for Money';
-const sellForCorp = 'Sell for Corporation Funds';
-const exchangeForCorpResearch = 'Exchange for Corporation Research';
+
+function getHashItem(arg: string) {
+  switch(arg) {
+    case 'money':
+    case '$':
+    default:
+      return 'Sell for Money';
+    case 'corp$':
+    case 'corpMoney':
+      return 'Sell for Corporation Funds';
+    case 'research':
+      return 'Exchange for Corporation Research';
+  }
+}
 
 export async function main(ns : NS) : Promise<void> {
-  const threshold = (ns.args[0] as number) || 30;
+  const purchasedItem = getHashItem(ns.args[0] as string);
   while (true) {
-    const nodesCount= new Array(ns.hacknet.numNodes()).fill(null);
-    const totalProduction = nodesCount.reduce((acc, val, i) => {
-      const currentNode = ns.hacknet.getNodeStats(i);
-      return acc + currentNode.production;
-    }, 0);
-    const purchasedItem = totalProduction > threshold ? exchangeForCorpResearch : exchangeForCorpResearch;
     if (ns.hacknet.numHashes() > ns.hacknet.hashCost(purchasedItem)) {
       while(ns.hacknet.numHashes() > ns.hacknet.hashCost(purchasedItem)) {
         ns.hacknet.spendHashes(purchasedItem);
