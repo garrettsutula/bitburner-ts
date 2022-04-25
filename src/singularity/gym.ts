18bceb8a204621de02afbd8bfaa282c1904cd557
Garrett Sutula <garrettsutula@gmail.com>
2022-04-24 23:17:00 -0400

    Add gym script

---
+++ src/singularity/gym.ts
@@ -0,0 +1,22 @@
+import { NS, Player } from '@ns'
+import { disableLogs } from '/helpers';
+
+function atTargetLevel(player: Player, targetLevel: number) {
+  return [player.agility, player.strength, player.dexterity, player.defense].every((skill) => targetLevel < skill);
+}
+
+export async function main(ns : NS) : Promise<void> {
+  disableLogs(ns, ['sleep']);
+  ns.tail();
+  const skills: Array<'agility' | 'strength' | 'defense' | 'dexterity'> = ['agility', 'strength', 'defense', 'dexterity'];
+  const targetLevel = (ns.args[0] as number) || 100;
+  while (!atTargetLevel(ns.getPlayer(), targetLevel)) {
+    if (ns.getPlayer()[skills[0]] >= targetLevel) {
+      skills.shift();
+      ns.singularity.gymWorkout('powerhouse gym', skills[0]);
+    } else {
+      if (!ns.singularity.isBusy()) ns.singularity.gymWorkout('powerhouse gym', skills[0]);
+    }
+    await ns.sleep(1000);
+  }
+} 
