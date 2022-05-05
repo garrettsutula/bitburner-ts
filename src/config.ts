3c435ef22fc152faeb3ac92e57d48a75aab5fd9f
Garrett Sutula <garrettsutula@gmail.com>
2022-05-04 21:30:58 -0400

    Adjust buffer

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,14 @@
 export const calculationParameters = {
   // Target % of money to hack from the server each procedure.
-  hackPercentage: 0.25,
+  hackPercentage: 0.80,
   // % of money to grow each round of prepare
-  prepareGrowthFactor: 1.5,
+  prepareGrowthFactor: 2,
   // % of money where we switch from 'prepare' to 'hack'
   maxServeryMoneyPercentage: 0.95,
   // % over minimum security allowed
   maxSecurityThreshold: 1.25,
   // Execution buffer between steps
-  stepBuffer: 20,
+  stepBuffer: 100,
 }
 
 export const schedulerParameters = {
@@ -21,7 +21,7 @@ export const schedulerParameters = {
   // When a server is done being prepared, we increase this by one to move on to the next host.
   baseAttackLimit: 1,
   // Buffer ensure procedure execution doesn't overlap.
-  executionBufferMs: 150,
+  executionBufferMs: 300,
   // When set to false, immediately target all hosts we can
   respectAttackLimit: false,
   reserveHomeRamGb: 16,
