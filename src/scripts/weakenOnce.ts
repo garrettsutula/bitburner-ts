f20bd4a8d54af337e83f897774a01af1f48fb5aa
Garrett Sutula <garrettsutula@gmail.com>
2022-05-10 14:11:37 -0400

    Script bugfix

--- src/scripts/weakenOnce.ts
+++ src/scripts/weakenOnce.ts
@@ -5,7 +5,7 @@ export async function main(ns : NS) : Promise<void> {
   const target = ns.args[0] as string;
   const delay = ns.args[1] as number;
   const processId = ns.args[2] as string;
-  const batchId = ns.args[2] as string;
+  const batchId = ns.args[3] as string;
   const monitorEnabled = ns.args[4] === 'monitor';
   await ns.sleep(delay);
   if (monitorEnabled) await writePortJson(ns, 2, {
