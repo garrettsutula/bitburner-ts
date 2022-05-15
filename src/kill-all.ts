9f931cd63cfbca81f6ece0a9477cc3469e7ffed4
Garrett Sutula <garrettsutula@gmail.com>
2022-05-15 10:40:38 -0400

    More aggressive killall script

--- src/kill-all.ts
+++ src/kill-all.ts
@@ -3,9 +3,9 @@ import { readJson } from '/lib/file';
 
 export async function main(ns : NS) : Promise<void> {
   const controlledHosts = readJson(ns, '/data/controlledHosts.txt') as string[];
-
+  const scripts = ns.ls('home', '/scripts/')
   controlledHosts.forEach((host) => {
-    host !== 'home' ? ns.killall(host): ns.scriptKill('/scripts/basic-hack.js', 'home');
+    host !== 'home' ? ns.killall(host): scripts.forEach((script) => ns.scriptKill(script, 'home'));
   });
   //
 }
