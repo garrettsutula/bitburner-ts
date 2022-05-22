9fe7b4b178a1acccf8b287671d1bef3dbf31c634
Garrett Sutula <garrettsutula@gmail.com>
2022-05-22 12:46:39 -0400

    Add server $ and sec to sell hashes

--- src/sell-hashes.ts
+++ src/sell-hashes.ts
@@ -1,7 +1,16 @@
 import { NS } from '@ns';
+const requiresTarget = [
+  'Reduce Minimum Security',
+  'Increase Maximum Money', 
+]
 
-function getHashItem(arg: string) {
-  switch(arg) {
+function getHashItem(ns: NS, name: string, target: string) {
+  if (requiresTarget.includes(name) && !target) {
+    ns.tprint(`ERROR: Command '${name}' requires second arg to specify target.`);
+    ns.exit();
+  }
+
+  switch(name) {
     case 'money':
     case '$':
     default:
@@ -16,15 +25,24 @@ function getHashItem(arg: string) {
     case 'university':
     case "uni":
       return "Improve Studying";
+    case "rank":
+      return "Exchange for Bladeburner Rank";
+    case 'sec':
+    case 'security':
+      return 'Reduce Minimum Security';
+    case 'serverMax':
+      return 'Increase Maximum Money';
   }
 }
 
 export async function main(ns : NS) : Promise<void> {
-  const purchasedItem = getHashItem(ns.args[0] as string);
+  ns.disableLog('sleep');
+  const [ name, target ] = ns.args as string[];
+  const purchasedItem = getHashItem(ns, name, target);
   while (true) {
     if (ns.hacknet.numHashes() > ns.hacknet.hashCost(purchasedItem)) {
       while(ns.hacknet.numHashes() > ns.hacknet.hashCost(purchasedItem)) {
-        ns.hacknet.spendHashes(purchasedItem);
+        ns.hacknet.spendHashes(purchasedItem, target);
       }
     }
     await ns.sleep(10000);
