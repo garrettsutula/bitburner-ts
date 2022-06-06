085041b86dfa1c453d5e83966a861f5640b1e519
Garrett Sutula <garrettsutula@gmail.com>
2022-06-06 11:19:05 -0400

    Killall bugfix

--- src/kill-all.ts
+++ src/kill-all.ts
@@ -1,11 +1,12 @@
 import { NS } from '@ns'
 import { readJson } from '/lib/file';
+import { ServerStats } from '/models/server';
 
 export async function main(ns : NS) : Promise<void> {
-  const controlledHosts = readJson(ns, '/data/controlledHosts.txt') as string[];
+  const controlledHosts = readJson(ns, '/data/serverStats.txt') as { [key: string]: ServerStats };
   const scripts = ns.ls('home', '/scripts/')
-  controlledHosts.forEach((host) => {
-    host !== 'home' ? ns.killall(host): scripts.forEach((script) => ns.scriptKill(script, 'home'));
+  Object.values(controlledHosts).forEach(({ name }) => {
+    name !== 'home' ? ns.killall(name): scripts.forEach((script) => ns.scriptKill(script, 'home'));
   });
   //
 }
